183cf225deaa42b71e24918d1c08315e058899cf
Azad <87714466+Azadbaku@users.noreply.github.com>
2022-07-03 02:36:32 +0400

    Updated by CrossUI No-Code App Builder

--- App/js/Setting.js
+++ App/js/Setting.js
@@ -9,27 +9,133 @@ xui.Class('App.Setting', 'xui.Module',{
                 .setHost(host,"ctl_pane10")
                 .setDock("fill")
                 .setShowEffects({
-                    "params":{
-                        "translateX":["100%","0%"]
+                    "params" : {
+                        "translateX" : [
+                            "100%",
+                            "0%"
+                        ]
                     },
-                    "type":"circIn",
-                    "duration":100
-                }
-                )
+                    "type" : "circIn",
+                    "duration" : 100
+                })
                 .setHideEffects({
-                    "params":{
-                        "translateX":["0%","100%"]
+                    "params" : {
+                        "translateX" : [
+                            "0%",
+                            "100%"
+                        ]
                     },
-                    "type":"circOut",
-                    "duration":100
-                }
-                )
+                    "type" : "circOut",
+                    "duration" : 100
+                })
                 .setConDockPadding({
-                    "left":32,
-                    "right":32,
-                    "bottom":0
-                }
-                )
+                    "left" : 32,
+                    "right" : 32,
+                    "bottom" : 0
+                })
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.ECharts")
+                .setHost(host,"xui_ui_echarts1")
+                .setLeft("0.6em")
+                .setTop("19.933333333333334em")
+                .setWidth("30.733333333333334em")
+                .setChartOption({
+                    "title" : {
+                        "text" : "Funnel",
+                        "subtext" : "Fake Data"
+                    },
+                    "tooltip" : {
+                        "trigger" : "item",
+                        "formatter" : "{a} <br/>{b} : {c}%"
+                    },
+                    "toolbox" : {
+                        "feature" : {
+                            "dataView" : {
+                                "readOnly" : false
+                            },
+                            "restore" : {
+                            },
+                            "saveAsImage" : {
+                            }
+                        }
+                    },
+                    "legend" : {
+                        "data" : [
+                            "Page View",
+                            "Click",
+                            "Visitor",
+                            "Consulting",
+                            "Order"
+                        ]
+                    },
+                    "calculable" : true,
+                    "series" : [
+                        {
+                            "name" : "Funnel",
+                            "type" : "funnel",
+                            "left" : "10%",
+                            "top" : 60,
+                            "bottom" : 60,
+                            "width" : "80%",
+                            "min" : 0,
+                            "max" : 100,
+                            "minSize" : "0%",
+                            "maxSize" : "100%",
+                            "sort" : "descending",
+                            "gap" : 2,
+                            "label" : {
+                                "normal" : {
+                                    "show" : true,
+                                    "position" : "inside"
+                                },
+                                "emphasis" : {
+                                    "textStyle" : {
+                                        "fontSize" : 20
+                                    }
+                                }
+                            },
+                            "labelLine" : {
+                                "normal" : {
+                                    "length" : 10,
+                                    "lineStyle" : {
+                                        "width" : 1,
+                                        "type" : "solid"
+                                    }
+                                }
+                            },
+                            "itemStyle" : {
+                                "normal" : {
+                                    "borderColor" : "#fff",
+                                    "borderWidth" : 1
+                                }
+                            },
+                            "data" : [
+                                {
+                                    "value" : 60,
+                                    "name" : "Visitor"
+                                },
+                                {
+                                    "value" : 40,
+                                    "name" : "Consulting"
+                                },
+                                {
+                                    "value" : 20,
+                                    "name" : "Order"
+                                },
+                                {
+                                    "value" : 80,
+                                    "name" : "Click"
+                                },
+                                {
+                                    "value" : 100,
+                                    "name" : "Page view"
+                                }
+                            ]
+                        }
+                    ]
+                })
             );
             
             host.ctl_pane10.append(
@@ -46,7 +152,7 @@ xui.Class('App.Setting', 'xui.Module',{
                 .setLabelCaption("Font Size")
                 .setLabelHAlign("left")
                 .setValue("23")
-                );
+            );
             
             host.ctl_pane10.append(
                 xui.create("xui.UI.ComboInput")
@@ -61,7 +167,7 @@ xui.Class('App.Setting', 'xui.Module',{
                 .setType("counter")
                 .setPrecision(0)
                 .setValue(12)
-                );
+            );
             
             host.ctl_pane10.append(
                 xui.create("xui.UI.Input")
@@ -77,7 +183,7 @@ xui.Class('App.Setting', 'xui.Module',{
                 .setLabelCaption("Memo")
                 .setLabelHAlign("left")
                 .setMultiLines(true)
-                );
+            );
             
             return children;
             // ]]Code created by CrossUI RAD Studio
